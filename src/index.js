99ee318b86925b5d0f4836c757abffc4a1121f36
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-04-27 12:40:50 +0300

    Update basic page layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import { homePage } from "./initial.js";
+import { layout, defaultContent, updateContent } from "./initial.js";
 import { mainMenu } from "./menu.js";
 
 function component() {
-  homePage();
+  layout();
+  defaultContent();
 
   const button = document.querySelector(".menu-button");
-
   button.addEventListener("click", () => {
-    mainMenu();
+    const element = document.querySelector(".new-content");
+
+    if (!element) {
+      mainMenu();
+      updateContent();
+    } else {
+      defaultContent();
+    }
   });
 }
 component();
--- src/initial.js
+++ src/initial.js
@@ -1,26 +1,60 @@
 import "./mainstyles.css";
 
-const homePage = () => {
+const layout = () => {
   const content = document.querySelector(".content");
+
   const header = document.createElement("div");
   const main = document.createElement("div");
   const footer = document.createElement("div");
   const menuButton = document.createElement("button");
-  const defaultContainer = document.createElement("div");
 
   header.classList.add("header");
   main.classList.add("main-section");
   footer.classList.add("footer");
   menuButton.classList.add("menu-button");
-  defaultContainer.classList.add("default-content");
 
   menuButton.textContent = "Main menu";
 
   header.appendChild(menuButton);
-  main.appendChild(defaultContainer);
   content.appendChild(header);
   content.appendChild(main);
   content.appendChild(footer);
 };
 
-export { homePage };
+const defaultContent = () => {
+  const container = document.querySelector(".main-section");
+
+  container.textContent = "";
+
+  const defaultContainer = document.createElement("div");
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
+  const heading = document.createElement("h2");
+
+  defaultContainer.classList.add("default-content");
+  heading.textContent = "Today";
+
+  div1.appendChild(heading);
+  defaultContainer.appendChild(div1);
+  defaultContainer.appendChild(div2);
+  container.appendChild(defaultContainer);
+};
+
+const updateContent = () => {
+  const container = document.querySelector(".new-content");
+
+  const defaultContainer = document.createElement("div");
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
+  const heading = document.createElement("h2");
+
+  defaultContainer.classList.add("default-content");
+  heading.textContent = "Today";
+
+  div1.appendChild(heading);
+  defaultContainer.appendChild(div1);
+  defaultContainer.appendChild(div2);
+  container.appendChild(defaultContainer);
+};
+
+export { layout, defaultContent, updateContent };
